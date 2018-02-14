a92c3354df215aab9317ce71069f59c72731b6e8
coinadvisor <35585437+coinadvisor@users.noreply.github.com>
2018-02-14 11:08:57 +0100

    Update CadvToken.sol

--- CadvToken.sol
+++ CadvToken.sol
@@ -61,7 +61,7 @@ contract BasicToken is ERC20Basic {
     * @dev Fix for the ERC20 short address attack.
     */
     modifier onlyPayloadSize(uint size) {
-        require(msg.data.length < size + 4);
+        require(msg.data.length >= size + 4);
         _;
     }
     
